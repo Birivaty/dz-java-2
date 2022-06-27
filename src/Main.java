cc707c1e7c9d8406f72be95339050d8964da1bb0
Birivaty <birivaty@yandex.ru>
2022-06-27 21:24:11 +0300

    all

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2000;
+        int bonus = 20;
+        System.out.println(amount / bonus);
+        int div = amount/bonus;
+    }
+}
